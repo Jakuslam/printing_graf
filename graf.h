f5f608275fa1e6a932decf97cdc629100377c3c6
Jakub Sláma <136487656+Jakuslam@users.noreply.github.com>
2026-04-05 20:12:26 +0200

    Fix formatting specifiers in graf.h

--- graf.h
+++ graf.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 
-#define TYPE_OF_GRAF size_t
-#define PRINT_OF_GRAF "%03zu | "
-#define PRINT_OF_GRAF_BUTTOM "%02zu "
+#define TYPE_OF_GRAF size_t       // defining the type of input data (must be a number)
+#define PRINT_OF_GRAF "%03zu | "      // change 'zu' to the correct format specifier
+#define PRINT_OF_GRAF_BUTTOM "%02zu " // change 'zu' to the correct format specifier
 
 
 size_t printGraph(TYPE_OF_GRAF iLenData, TYPE_OF_GRAF iData[]);
